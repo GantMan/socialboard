b9e3ae5c44d28d81e4b0bec200691bd6eeaf6136
Gant <GantMan@gmail.com>
2014-05-10 00:59:44 -0500

    annotated models

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  subtitle   :string(255)
+#  autoshow   :boolean
+#  delay      :integer
+#  repeat     :boolean
+#  curated    :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#  keywords   :text             default("[]"), not null
+#  blacklist  :text             default("[]"), not null
+#
+
 class Board < ActiveRecord::Base
   serialize :keywords, JSON
   serialize :blacklist, JSON
