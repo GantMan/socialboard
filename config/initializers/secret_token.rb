cbb0bbb8b4b4c3de230cd269329960ea19347c32
Gant <GantMan@gmail.com>
2014-05-09 21:47:24 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialboard::Application.config.secret_key_base = '0d1c26514acc436a7769fb51401f288c7784f8856ce563475991c1640c4afdc2da1a8b17e5ec876c0b8618130d4efd359f6eceb7f8b23c865d4c369ddee87de1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialboard::Application.config.session_store :cookie_store, key: '_socialboard_session'
